cc3e5b5a79249a04bae9d040cfa925e626a9f47c
jyi0226 <105589421+jyi0226@users.noreply.github.com>
2023-04-04 15:06:44 +0900

    Create _3_3.java

---
+++ _3_3.java
@@ -0,0 +1,7 @@
+public class _3_3 {
+    public static void main(String[] args) {
+        String s="881120-1068234";
+        System.out.println(19+s.substring(0,6));
+        System.out.println(s.substring(7));
+    }
+}
