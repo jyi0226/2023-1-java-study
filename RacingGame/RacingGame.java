724dc5c4885ef04b262f7d8f39c208eb4556785a
Youngin Jeong <105589421+jyi0226@users.noreply.github.com>
2023-05-30 13:06:53 +0900

    RacingGame 클래스
    메인함수

---
+++ RacingGame/RacingGame.java
@@ -0,0 +1,13 @@
+package RacingGame;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class RacingGame {
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.startGame();
+    }
+}
